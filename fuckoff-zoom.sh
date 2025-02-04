164f989d5282da7440e9125661ba4f012c846149
Dmitry Dubina <dmitryd.prog@gmail.com>
2025-02-04 16:45:21 +0700

    Update fuckoff-zoom.sh

--- fuckoff-zoom.sh
+++ fuckoff-zoom.sh
@@ -1,17 +1,20 @@
 #! /usr/bin/env bash
 
 wins=`xdotool search "as_toolbar"`
+floating_bar_developer_mom_ass=-10000
 
 for win in $wins
 do
-        xprop -id $win | grep WM_CLASS | grep zoom
-        if [ $? = 0 ]; then
-                if [ `cat /dev/shm/zoom_toolbar` = "fuckoffed" ]; then
-                        xdotool windowmove --sync $win 0 0
-                        echo "here" > /dev/shm/zoom_toolbar
-                else
-                        xdotool windowmove --sync $win -10000 -10000
-                        echo "fuckoffed" > /dev/shm/zoom_toolbar
-                fi
-        fi
+  xprop -id $win | grep WM_CLASS | grep zoom
+  if [ $? = 0 ]; then
+    if [ `cat /dev/shm/zoom_toolbar` = "fuckoffed" ]; then
+      xdotool windowmove --sync $win 0 0
+      echo "here" > /dev/shm/zoom_toolbar
+      kdialog --title "Zoom bar: show" --passivepopup "" 1
+    else
+      xdotool windowmove --sync $win $floating_bar_developer_mom_ass $floating_bar_developer_mom_ass
+      echo "fuckoffed" > /dev/shm/zoom_toolbar
+      kdialog --title "Zoom bar: hide" --passivepopup "" 1
+    fi
+  fi
 done
